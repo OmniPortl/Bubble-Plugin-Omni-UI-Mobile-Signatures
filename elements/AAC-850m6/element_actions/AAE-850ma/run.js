99f117d1ae3795c1a94ce63450672b73b0c73464
OmniPortl <150485211+OmniPortl@users.noreply.github.com>
2026-08-03 14:18:54 -0700

    Automatic commit from Bubble

--- elements/AAC-850m6/element_actions/AAE-850ma/run.js
+++ elements/AAC-850m6/element_actions/AAE-850ma/run.js
@@ -1,6 +1,11 @@
 function(instance, properties, context) {
-  var runtime = instance.data && instance.data.omniSignatureRuntime;
-  if (runtime && typeof runtime.saveSignature === "function") {
-    runtime.saveSignature(context || {});
-  }
+    instance.data = instance.data || {};
+    if (typeof instance.data.omniSignatureBootstrap === "function") {
+        instance.data.omniSignatureBootstrap();
+    }
+
+    var runtime = instance.data.omniSignatureRuntime || instance.data.omniSignatureMobileRuntime;
+    if (runtime && runtime.helpers && typeof runtime.helpers.saveSignature === "function") {
+        runtime.helpers.saveSignature(context);
+    }
 }
--- elements/AAC-850m6/element_actions/AAG-850me/run.js
+++ elements/AAC-850m6/element_actions/AAG-850me/run.js
@@ -1,6 +1,11 @@
 function(instance, properties, context) {
-  var runtime = instance.data && instance.data.omniSignatureRuntime;
-  if (runtime && typeof runtime.undoLastStroke === "function") {
-    runtime.undoLastStroke();
-  }
+    instance.data = instance.data || {};
+    if (typeof instance.data.omniSignatureBootstrap === "function") {
+        instance.data.omniSignatureBootstrap();
+    }
+
+    var runtime = instance.data.omniSignatureRuntime || instance.data.omniSignatureMobileRuntime;
+    if (runtime && runtime.helpers && typeof runtime.helpers.undoLastStroke === "function") {
+        runtime.helpers.undoLastStroke(context);
+    }
 }
